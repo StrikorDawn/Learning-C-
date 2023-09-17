ca5735ae596e4fe9df05ef1530d602091c62eff5
StrikorDawnh <tadawg007@byui.edu>
2023-09-16 18:52:01 -0600

    Messing arround

--- testing/Program.cs
+++ testing/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This is in C#");
+
+int number = 5;
+number = 8;
+number = number + 3;
+
+Console.WriteLine("What is your favorite color? ");
+string color = Console.ReadLine();
+Console.WriteLine($"Your color is {color}");
