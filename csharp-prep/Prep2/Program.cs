968207e8bb17fc019358b64b356f7fdf587db953
StrikorDawnh <tadawg007@byui.edu>
2023-09-16 19:55:30 -0600

    Completed prep assignments 1 & 2!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage? ");
+        string user_input = Console.ReadLine();
+        int user_percentage = int.Parse(user_input);
+        int last_diget = user_percentage % 10;
+        string grade_symbole = "";
+        string user_grade;
+        if (user_percentage <= 100)
+        {
+            if (last_diget >= 7)
+            {
+                grade_symbole = "+";
+            }
+            else if (last_diget <= 3)
+            {
+                grade_symbole = "-";
+            }
+
+            if (user_percentage >= 97)
+            {
+                user_grade = "A";
+                grade_symbole = "";
+            }
+            else if (user_percentage >= 90 )
+            {
+                user_grade = "A";
+            }
+            else if (user_percentage >= 80)
+            {
+                user_grade = "B";
+            }
+            else if (user_percentage >= 70)
+            {
+                user_grade = "C";
+            }
+            else if (user_percentage >= 60)
+            {
+                user_grade = "D";
+            }
+            else 
+            {
+                user_grade = "F";
+                grade_symbole = "";
+
+            }
+
+            if (user_percentage >= 70)
+            {
+            Console.WriteLine($"Your letter grade is {grade_symbole}{user_grade}, Congrats you passed!");
+            }
+            else{
+            Console.WriteLine($"Your letter grade is {grade_symbole}{user_grade}, looks like I'll be seeing you again next semester.");
+            }
+        }
+        else{
+            Console.WriteLine("The percentage you entered was in valid, please try agian.");
+        }
     }
 }
