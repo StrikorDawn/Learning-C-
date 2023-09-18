8b863c9217dcc96f3882d4a8b6a0e4b7956d2445
StrikorDawnh <tadawg007@byui.edu>
2023-09-18 10:00:37 -0600

    Started the prep 3 assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Net;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int min_number = 1;
+        int max_number = 11;
+        string user_response = "";
+        int user_guess = -1;
+        do{
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(min_number,max_number);
+        Console.WriteLine($"Guess the magic number between {min_number} and {max_number}.");
+            do{
+                Console.Write("What is the magic number? ");
+            } while (user_guess != magic_number);
+        Console.Write("Would you like to play again? ");
+        user_response = Console.ReadLine();
+        } while (user_response == "yes");
+        
     }
 }
