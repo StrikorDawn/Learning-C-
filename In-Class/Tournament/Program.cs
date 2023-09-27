31fd20726de4f05f1c9cea0cb89fe401e395adae
StrikorDawnh <tadawg007@byui.edu>
2023-09-27 10:00:50 -0600

    Made a Tournament program

---
+++ In-Class/Tournament/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Team Mario = new Team("Mario");
+Player ronaldo = new Player("Chrisiano Ronaldo", 7);
+Player taden = new Player("Taden Marston", 47);
+
+Mario.AddPlayer(taden);
+Mario.AddPlayer(ronaldo);
+
+Mario.DisplayRoster();
